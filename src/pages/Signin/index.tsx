444fc19be89db31055a1c9e9972039713cd962f7
Sandro Santos <sandro.dev@gmail.com>
2021-02-03 23:26:35 -0300

    feat: initial pages

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Signin: React.FC = () => {
+  return (
+    <h1>Signin</h1>
+  );
+}
+
+export default Signin;
---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Signup: React.FC = () => {
+  return (
+    <h1>Signup</h1>
+  );
+}
+
+export default Signup;
