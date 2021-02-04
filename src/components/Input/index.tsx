244440a059d6b5e90ad6c8926958d56be00444d8
Sandro Santos <sandro.dev@gmail.com>
2021-02-04 18:25:47 -0300

    feat: Unform library to control Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,31 @@
-import React, {InputHTMLAttributes} from 'react';
+import React, {InputHTMLAttributes, useEffect, useRef, useState} from 'react';
 import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
+
+import { useField } from '@unform/core';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({icon: Icon, ...props}) => {
+const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
+
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input type="text" {...props}/>
+      <input ref={inputRef} type="text" defaultValue={defaultValue} {...rest}/>
     </Container>
   );
 }
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo-gobarber.svg';
 import signinBackground from '../../assets/signin-background.svg';
@@ -10,25 +13,40 @@ import Input from '../../components/Input';
 import { Container, Content, BackgroundImage } from './styles';
 
 const Signin: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback((data: Object) => {
+    console.log(data);    
+  }, []);
+
   return (
     <Container>
-      <Content>
+      <Content>        
         <img src={logoImg} alt="logo"/>
-        <form action="">
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
+
           <Input name="email" type="email" icon={FiMail} placeholder="E-mail"/>
-          <Input name="password" type="password" icon={FiLock} placeholder="Senha"/>
+          <Input name="password" type="password" icon={FiLock} placeholder="Senha"/>          
           <Button type="submit">Entrar</Button>
+          
           <a href="/forgot">Esqueci minha senha</a>
-        </form>
+
+        </Form>
+
         <a href="/signup">
           <FiLogIn size={16} />
           Criar conta
         </a>
+
       </Content>
+
       <BackgroundImage>
         <img src={signinBackground} alt="Background"/>
       </BackgroundImage>
+
     </Container>
   );
 }
