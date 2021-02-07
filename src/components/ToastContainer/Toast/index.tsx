5bc0545a8faaa3ebc0426144eaf601c6d0bbea7a
Sandro Santos <sandro.dev@gmail.com>
2021-02-07 20:12:50 -0300

    feat: removeToast with timeout

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiAlertCircle, FiXCircle} from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/Toast';
@@ -12,6 +12,17 @@ interface ToastProps {
 const Toast: React.FC<ToastProps> = ({ message }) => {
 
   const {removeToast} = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, [message.id, removeToast]);
 
   return (
     <Container type={message.type} hasDescription={!!message.description} >
