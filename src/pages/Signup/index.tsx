babd5b7fe4d794aaedec394048da6069e9d42262
Sandro Santos <sandro.dev@gmail.com>
2021-02-08 10:49:11 -0300

    feat: signup form to create user and toasts

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -5,11 +5,14 @@ import { Link } from 'react-router-dom';
 
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
+import { useToast } from '../../hooks/Toast';
 
 import logoImg from '../../assets/logo-gobarber.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import getValidationErrors from '../../utils/getValidationErrors';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -18,27 +21,59 @@ import {
   BackgroundImage,
 } from './styles';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const { addToast } = useToast();
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('O nome é obrigatório'),
-        email: Yup.string()
-          .required('O email é obrigatório')
-          .email('Insira um email válido'),
-        password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('O nome é obrigatório'),
+          email: Yup.string()
+            .required('O email é obrigatório')
+            .email('Insira um email válido'),
+          password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        await api.post('/users', {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description:
+            'Ocorreu um erro ao tentar realizar o cadastro, verifique as informações',
+        });
+
+        return;
+      }
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado',
+        description: 'Cadastro foi realizado com sucesso!',
       });
-
-      await schema.validate(data, { abortEarly: false });
-    } catch (err) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+    },
+    [addToast],
+  );
 
   return (
     <Container>
