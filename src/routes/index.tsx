80bfb05825f67f16bbd5edb9a12c232ab2ee05a6
Sandro Santos <sandro.dev@gmail.com>
2021-02-09 11:00:30 -0300

    feat: verify is user is authenticated for private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
+import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-const routes: React.FC = () => {
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => {
   return (
     <Switch>
-      {/* <Route path="/" exact component={Dashboard} /> */}
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
 
-export default routes;
+export default Routes;
