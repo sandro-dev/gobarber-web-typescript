8f5c6bb4848b253a3f919a41d464e275416a59af
Sandro Santos <sandro.dev@gmail.com>
2021-02-15 17:40:09 -0300

    test: toast hook

--- src/__tests__/hooks/Toast.spec.tsx
+++ src/__tests__/hooks/Toast.spec.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { ToastProvider, useToast } from '../../hooks/Toast';
+import { render, waitFor } from '@testing-library/react';
+import { useToast, ToastProvider, ToastMessage } from '../../hooks/Toast';
+
+import ToastContainer from '../../components/ToastContainer';
 
 const mockedAddToast = jest.fn();
+const mockedRemoveToast = jest.fn();
 
 jest.mock('../../hooks/Toast', () => {
   return {
     useToast: () => ({
       addToast: mockedAddToast,
+      removeToast: mockedRemoveToast,
     }),
   };
 });
@@ -17,18 +23,43 @@ describe('Toast hook', () => {
       wrapper: ToastProvider,
     });
 
-    act(() => {
+    act(() =>
       result.current.addToast({
         type: 'success',
         title: 'Tested with success',
         description: 'This function test how to show toast on pages',
-      });
-    });
-
-    expect(mockedAddToast).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: 'success',
       }),
     );
+
+    const messages: ToastMessage[] = [
+      {
+        id: 'id-123',
+        type: 'success',
+        title: 'success',
+        description: 'The credentials are correts',
+      },
+      {
+        id: 'id-456',
+        type: 'error',
+        title: 'error',
+        description: 'Fails on try to authenticate. Verify the credentials',
+      },
+      {
+        id: 'id-789',
+        title: 'info',
+        description: 'Just a toast information',
+      },
+    ];
+
+    const { getAllByText } = render(<ToastContainer messages={messages} />);
+
+    const toastElement = getAllByText('success');
+    expect(toastElement).toBeTruthy();
+
+    await waitFor(() => expect(mockedRemoveToast).toHaveBeenCalled(), {
+      timeout: 4000,
+    });
+
+    // debug();
   });
 });
